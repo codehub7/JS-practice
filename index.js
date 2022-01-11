18f87c94cd8e739ad60c663d3b121b6ec7758828
Rezwan <rezwan.kaoser@enosisbd.com>
2022-01-11 16:52:51 +0600

    datatype pratice done primarily

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+'use strict'
 console.log("Hello world")
+console.log(393429235423523523455342398234890n*2392323234452354239232323234n)
+console.log(typeof 3n)
+console.log(typeof 23423)
+console.log(typeof "H")
+
+let name = "Rezwan"
+console.log(`My Name is ${name}`)
