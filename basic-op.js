e5e3f8bede0d8053ac435f4b880017c4a0060f7e
Rezwan <rezwan.kaoser@enosisbd.com>
2022-01-11 18:09:27 +0600

    basic operation math

---
+++ basic-op.js
@@ -0,0 +1,18 @@
+let x = "32"
+
+console.log(typeof x)
+console.log(typeof +x)
+console.log(typeof -x)
+
+console.log("" + 1 + 0)  // '10'
+console.log("" - 1 + 0)  // -1
+console.log(true+false)  // 1
+
+console.log("   -9   "+4)  //    -9   4
+console.log("   -9   "-4)  // -13
+
+
+console.log(null + 1) // 1
+console.log(undefined + 1) //NaN
+
+console.log("  \t\n "-2)
