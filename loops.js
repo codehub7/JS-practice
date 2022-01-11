f49f008f31328cee7689069dc9e026d573fbd67a
Rezwan <rezwan.kaoser@enosisbd.com>
2022-01-11 21:17:06 +0600

    label for break/continue

--- loops.js
+++ loops.js
@@ -1,11 +1,21 @@
-let i = 10;
-while(i){
-    console.log(i);
-    i--;
-}
+// let i = 10;
+// while(i){
+//     console.log(i);
+//     i--;
+// }
 
 
-console.log("FOR LOOP")
-for ( i = 0 ; i < 3; i++) { 
-  console.log( i ); // 0, 1, 2
+// console.log("FOR LOOP")
+// for ( i = 0 ; i < 3; i++) { 
+//   console.log( i ); // 0, 1, 2
+// }
+
+// label for break/continue
+outer: for (let i = 0;i<3; i++){
+    for(let j = 0 ;j<3;j++){
+        let input = prompt(`Value of the coordinate: (${i}, ${j})`, '')
+        if(!input){
+            break outer;
+        }
+    }
 }
